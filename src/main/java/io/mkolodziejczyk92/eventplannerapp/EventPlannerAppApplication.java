57d34b5f797574b90b73ac63d3b30c5573fd27f5
mkolodziejczyk92 <marcin.kdz92@gmail.com>
2023-05-14 18:48:31 +0200

    First commit

---
+++ src/main/java/io/mkolodziejczyk92/eventplannerapp/EventPlannerAppApplication.java
@@ -0,0 +1,13 @@
+package io.mkolodziejczyk92.eventplannerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlannerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPlannerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mkolodziejczyk92/eventplannerapp/EventPlannerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mkolodziejczyk92.eventplannerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlannerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
